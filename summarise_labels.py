055fa42f1252f236fb91c1d184c853ea1d94c778
Tanmay Rai Nanda <Stormrider7600@gmail.com>
2024-03-12 13:52:03 +0530

    Updated CSV

--- summarise_labels.py
+++ summarise_labels.py
@@ -72,5 +72,5 @@ def add_summaries(df_subset,mode='a'):
     df_subset.to_csv('data_with_summaries.csv', mode=mode, header=False if mode == 'a' else True, index=False, encoding='utf-8')
 
 # Call the functions
-df_subset = copy_rows(3500,4000)
+df_subset = copy_rows(4000,4500)
 add_summaries(df_subset, mode='a')
