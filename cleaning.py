8d79ab61f308338a0c3e582cd11814aa30e73717
shivangiag08 <shivangiag08@gmail.com>
2024-03-14 16:44:48 +0530

    'adding ssl model to the project'

--- cleaning.py
+++ cleaning.py
@@ -1,21 +1,26 @@
 import pandas as pd
 
-file_path = "project/news-more-context/global news data set/rating.csv"
+global_news_data = pd.read_csv("C:\Shivangi\college\Sem 4\MLPR\project/news-more-context\global news data set/rating.csv")
+# drop the columns that are not required
+global_news_data = global_news_data[['article_id', 'source_id', 'source_name', 'title','published_at', 'category', 'article', 'title_sentiment']]
+# split global news into training and testing data. use sklearn.model_selection.train_test_split
+from sklearn.model_selection import train_test_split
+train, test = train_test_split(global_news_data, test_size=0.2)
+print(train.shape)
+# build a co training classifier
+from sklearn.semi_supervised import LabelPropagation
+from sklearn.semi_supervised import LabelSpreading
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import make_pipeline
 
-def remove_rows(file_path):
-
-    df = pd.read_csv(file_path, encoding_errors='ignore')
-    print(len(df))
-    # print rows 75-85
-    print(df[9843:9873])
-    # only take rows where title_sentiment == Positive or Negative or Neutral. use inplace to modify the dataframe
-    return df
-
-# write the dataframe to a new csv file
-def write_to_csv(df):
-    df.to_csv('project/news-more-context/global news data set/ratings_cleaned.csv', index=False)
-
-df = remove_rows(file_path)
-print("Dataframe cleaned.")
-print("Dataframe written to csv.")
-print(len(df))
+# create a pipeline
+model = make_pipeline(TfidfVectorizer(), LabelSpreading())
+model.fit(train['article'], train['title_sentiment'])
+# predict the sentiment of the test data
+predicted_sentiment = model.predict(test['article'])
+print("predicted sentiment: ", predicted_sentiment.shape)
+# calculate the accuracy of the model
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+print(accuracy_score(test['title_sentiment'], predicted_sentiment))
+print(confusion_matrix(test['title_sentiment'], predicted_sentiment))
+print(classification_report(test['title_sentiment'], predicted_sentiment))
