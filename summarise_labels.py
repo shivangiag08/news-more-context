73c72cfc5e48d57c09560fb31b33c14a51c66ab6
Tanmay Rai Nanda <Stormrider7600@gmail.com>
2024-04-14 20:04:47 +0530

    updated gemini config file

--- summarise_labels.py
+++ summarise_labels.py
@@ -72,5 +72,5 @@ def add_summaries(df_subset,mode='a'):
     df_subset.to_csv('data_with_summaries.csv', mode=mode, header=False if mode == 'a' else True, index=False, encoding='utf-8')
 
 # Call the functions
-df_subset = copy_rows(18000,22000)
+df_subset = copy_rows(22000,23000)
 add_summaries(df_subset, mode='a')
